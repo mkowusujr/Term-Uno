c34fa08f86135f2b68825fd6a2559e73666a6b68
mkowusujr <mrfriiday@gmail.com>
2022-01-04 01:52:31 -0500

    Added equals function and tested it

--- src/Card.java
+++ src/Card.java
@@ -1,28 +1,41 @@
-public class Card{
+public class Card {
     private char color;
     private String value;
     private boolean faceUp;
 
-    public Card(char color, String value){
+    public Card(char color, String value) {
         this.color = color;
         this.value = value;
         faceUp = false;
     }
 
-    public boolean isFaceUp(){
+    public boolean isFaceUp() {
         return faceUp;
     }
 
-    public void flipCard(Card card){
+    public void flipCard(Card card) {
         card.faceUp = !card.faceUp;
     }
 
-    public void cardAffect(Card Card){
+    public void cardAffect(Card Card) {
         // TODO Switch class to do some thing to the deck
         // TODO Maybe add Deck Object, or Game Object idk rn
     }
 
-    public String toString(){
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Card) {
+            Card lastPlayed = (Card) other;
+            if (this.color == lastPlayed.color) {
+                return true;
+            }
+            return this.value.equals(lastPlayed.value);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
         return color + value;
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,29 @@
-public class Game{
+public class Game {
     public static void main(String[] args) {
         // Testing Card Class
         Card[] deck = {
-            new Card('r', "1"),
-            new Card('r', "2"),
-            new Card('b', "1"),
-            new Card('y', "+2")
+                new Card('r', "1"),
+                new Card('r', "2"),
+                new Card('b', "1"),
+                new Card('y', "+2")
         };
         for (Card card : deck) {
             System.out.print(card + "  ");
         }
         System.out.print("\n");
+
+        System.out.print("Are r4 and y4 equal?");
+        Card red = new Card('r', "4");
+        Card blue = new Card('b', "4");
+        System.out.println("Are r4 and y4 equal? " + red.equals(blue));
+        Card green = new Card('g', "4");
+        Card greendos = new Card('g', "7");
+        System.out.println("Are g4 and g7 equal? " + green.equals(greendos));
+
+        // Check for False
+        red = new Card('r', "5");
+        blue = new Card('b', "4");
+        System.out.println("Are r5 and y4 equal? " + red.equals(blue));
+
     }
 }
