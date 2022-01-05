4021380f04ddef4f7319810b8fff8b1cf11cfc47
mkowusujr <mrfriiday@gmail.com>
2022-01-05 16:07:12 -0500

    Added special cards, simulates a game

--- src/Game.java
+++ src/Game.java
@@ -40,20 +40,82 @@ public class Game {
         }
     }
     
-    private void aiMove(){
-        // TODO
-    }
+    // private void aiMove(){
+    //     // TODO write ai
+    // }
     
     private int nextPlayer(int lastPos){
-        return 0;
+        int currentPos = lastPos;
+        if (clockwise == true){ // go right
+            if (lastPos + 1 == players.length){
+                currentPos = 0;
+            }else{
+                currentPos += 1;
+            }
+        }else{ // go left
+            if(lastPos - 1 < 0){ 
+                currentPos = players.length - 1;
+            }else{
+                currentPos -= 1;
+            }
+        }
+        return currentPos;
     }
 
     private int whoGoesNext(int lastPos, boolean skip){
-        return 0;
+        int currentPos = nextPlayer(lastPos);
+        if (skip == true){
+            return whoGoesNext(currentPos, false);
+        }else{
+            return currentPos;
+        }
     }
 
-    public String playGame(int startingPlayer){
-        return "";
+    public String playGame(int startingPlayer, Scanner kb){
+        boolean skip = false;
+        int nextPos;
+        // make player move
+        Player player = players[startingPlayer];
+        humanMove(player, kb);
+        Card cardPlayed = gameDeck.getTopOfDiscardDeck();
+        int topCardVal = cardPlayed.getValue();
+        if (topCardVal > 9) { // Special card played
+            int nextPlayer;
+            ArrayList<Card> deltCards;
+            switch(topCardVal){
+                case 10: // skip card
+                skip = true;
+                    break;
+                case 11: // reverse card
+                    clockwise = !clockwise;
+                    break;
+                case 12: // draw two card
+                    nextPlayer = nextPlayer(startingPlayer);
+                    deltCards = gameDeck.dealPlayingDeck(2);
+                    players[nextPlayer].receiveHand(deltCards);
+                    skip = true;
+                    break;
+                case 13: // wild card
+                    break;
+                case 14: // draw 4 wild card
+                    nextPlayer = nextPlayer(startingPlayer);
+                    deltCards = gameDeck.dealPlayingDeck(4);
+                    players[nextPlayer].receiveHand(deltCards);
+                    skip = true;
+                    break;
+            }
+        }
+        if (player.getHandCount() == 1){
+            // TODO UNO
+            System.out.println("UNO!");
+            nextPos = whoGoesNext(startingPlayer, skip);
+            return playGame(nextPos, kb);
+        }else if (player.getHandCount() == 0){
+            return "player " + startingPlayer + " wins!";
+        }else{
+            nextPos = whoGoesNext(startingPlayer, skip);
+            return playGame(nextPos, kb);
+        }
     }
 
     public int currentRound() {
@@ -64,11 +126,7 @@ public class Game {
         Game newGame = new Game(2, 7);
         Scanner kb = new Scanner(System.in);
         System.out.println("Round " + newGame.currentRound());
-        while(true){
-            newGame.humanPlay(newGame.players[0], kb);
-            if (newGame.players[0].getHandCount() == 0)
-                break;
-        }
+        newGame.playGame(0, kb);
         kb.close();
     }
 }
