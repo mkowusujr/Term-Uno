b22225223d6ecbb597c32c1488ff7a12da3c3463
mkowusujr <mrfriiday@gmail.com>
2022-07-01 11:06:00 -0400

    doc: Added doc comments to Card class and refactor long if else statements with switch case

--- src/Card.java
+++ src/Card.java
@@ -1,47 +1,111 @@
+/**
+ * The Uno Card Class
+ * 
+ * @author Mathew Owusu Jr
+ */
 public class Card {
     private char color;
     private int value;
     private boolean faceUp;
-
+    
+    /**
+     * Class constructor for creating an uno card
+     * 
+     * @param color is the first letter of the card's color
+     *      acceptable color values are 'r' for red,
+     *      'b' for blue, 'g' for green, 'y' for yellow, and
+     *      's' for wild cards
+     * @param value a number representing what kind of card this is
+     *      values 1-9 are uno cards 1-9
+     *      value 10 represents a skip card, 11 is a reverse card,
+     *      12 is a plus two card, 13 is a wild card,
+     *      and 14 is a plus four card
+     */
     public Card(char color, int value) {
         this.color = color;
         this.value = value;
         faceUp = false;
     }
 
+    /**
+     * Checks if the card color and type face is up
+     * @return A boolean representing if the card's face is up(true) 
+     *      or not(false)
+     */
     public boolean isFaceUp() {
-        if (faceUp)
-            return true;
-        else
-            return faceUp;
+        return faceUp;
     }
 
+    /**
+     * Flips the card
+     */
     public void flipCard() {
         faceUp = !faceUp;
     }
 
+    /**
+     * Gets the cards color
+     * @return A char representing the card's color
+     */
     public char getColor() {
         return color;
     }
 
+    /**
+     * Gets the cards value
+     * @return A number representing what tyoe of card this is
+     *      values 1-9 are uno cards 1-9
+     *      value 10 represents a skip card, 11 is a reverse card,
+     *      12 is a plus two card, 13 is a wild card,
+     *      and 14 is a plus four card
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Changes the card's current color, only works on cards with a 
+     * value greater than 12. This means you can only change the color
+     * of a wild card, which has a value of 13, and a plus four card,
+     * which has a value of 14
+     * @param color is the first letter of the card's color
+     *      acceptable color values are 'r' for red,
+     *      'b' for blue, 'g' for green, 'y' for yellow, and
+     *      's' for wild cards
+     */
     public void changeColor(char color) {
         if (value > 12) {
             this.color = color;
         }
     }
 
+    /**
+     * Changes the card's current color, only works on cards with a 
+     * value greater than 12. This means you can only change the color
+     * of a wild card, which has a value of 13, and a plus four card,
+     * which has a value of 14. This function is for cases were the user types
+     * in the colors full name
+     * @param color is the first letter of the card's color
+     *      acceptable color values are 'r' for red,
+     *      'b' for blue, 'g' for green, 'y' for yellow, and
+     *      's' for wild cards
+     */
     public void changeColor(String color) {
         if (value > 12) {
             this.color = color.charAt(0);
         }
     }
 
-    public boolean canPlayCard(Deck playingDeck, Player player) {
-        if (this.equals(playingDeck.getTopOfDiscardDeck())) {
+    /**
+     * Whether or not this card can be played on top of the discard deck
+     * @param discardDeck The deck of cards being used to store the cards
+     *      being discarded during the game
+     * @param player the player playing the current card
+     * @return A boolean representing whether or not this card matches the
+     *      card on top of the discard deck
+     */
+    public boolean canPlayCard(Deck discardDeck, Player player) {
+        if (this.equals(discardDeck.getTopOfDiscardDeck())) {
             String pType;
             if (player.isHuman()) {
                 pType = "You played";
@@ -49,7 +113,7 @@ public class Card {
                 pType = "Played";
             }
             System.out.printf("%s a %s card\n", pType, this);
-            playingDeck.addToDiscardCard(this);
+            discardDeck.addToDiscardCard(this);
             return true;
         } else {
             player.receiveHand(this);
@@ -57,6 +121,11 @@ public class Card {
         }
     }
 
+    /**
+     * Checks if card has either the same color or card value 
+     * as another card. If the other card is the special card
+     * Wild Card or Plus Four Card, then it will pass the check
+     */
     @Override
     public boolean equals(Object other) {
         if (other instanceof Card) {
@@ -69,25 +138,36 @@ public class Card {
         return false;
     }
 
+    /**
+     * Converts the card values to a char string representing
+     * it's card type
+     * @return A string representing the card's current face
+     */
     public String stringVal() {
         if (isFaceUp() == true) {
-            if (value == 10)
-                return color + "x";
-            else if (value == 11)
-                return color + "<>";
-            else if (value == 12)
-                return color + "+2";
-            else if (value == 13)
-                return color + "w";
-            else if (value == 14)
-                return color + "+4";
-            else
-                return color + String.valueOf(value);
+            switch(value) {
+                case 10:
+                    return color + "x";
+                case 11:
+                    return color + "<>";
+                case 12:
+                    return color + "+2";
+                case 13:
+                    return color + "w";
+                case 14:
+                    return color + "+4";
+                default:
+                    return color + String.valueOf(value);
+            }
         } else {
             return "u";
         }
     }
 
+    /**
+     * Displays the card to the terminal. This function is responsible
+     * for giving each card its respective color
+     */
     @Override
     public String toString() {
         String output = "";
@@ -110,19 +190,21 @@ public class Card {
                     break;
             }
         }
-        if (isFaceUp() == true) {
-            if (value == 10)
-                return output + color + "x" + "\033[0m";
-            else if (value == 11)
-                return output + color + "<>" + "\033[0m";
-            else if (value == 12)
-                return output + color + "+2" + "\033[0m";
-            else if (value == 13)
-                return output + color + "w" + "\033[0m";
-            else if (value == 14)
-                return output + color + "+4" + "\033[0m";
-            else
-                return output + color + String.valueOf(value) + "\033[0m";
+        if (isFaceUp() == true) {  // prints the card face if its up
+            switch(value) {
+                case 10:
+                    return output + color + "x" + "\033[0m";
+                case 11:
+                    return output + color + "<>" + "\033[0m";
+                case 12:
+                    return output + color + "+2" + "\033[0m";
+                case 13:
+                    return output + color + "w" + "\033[0m";
+                case 14:
+                    return output + color + "+4" + "\033[0m";
+                default:
+                    return output + color + String.valueOf(value) + "\033[0m";
+            }
         } else {
             return "\033[37;1mu\033[0m";
         }
