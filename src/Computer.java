253269eacf06258af120240bfae24a91d0524390
mkowusujr <mrfriiday@gmail.com>
2022-07-02 19:50:48 -0400

    docs: added docments to computer class

--- src/Computer.java
+++ src/Computer.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.lang.Thread;
 
 /**
- * 
+ * A Class representing a computer player
  * 
  * @author Mathew Owusu Jr
  */
@@ -11,96 +11,169 @@ public class Computer extends Player {
         super();
     }
 
+    /**
+     * Introduces delay to the computer player's moves to make the game easier
+     * to follow in the terminal
+     */
     private void think() {
         try {
             int thinkTime = 0;
-            thinkTime = (int) Math.random() * (3000 - 500 + 1) + 500;
+
+            // Random time in milliseconds in range of 500 ms to 2500 ms inclusive
+            thinkTime = (int) Math.random() * (2500 + 1) + 500;
             Thread.sleep(thinkTime);
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    @Override
-    Card playCard(PlayingDeck playingDeck, DiscardDeck discardDeck) {
-        Card lastPlayed = discardDeck.getTopOfDeck();
-        // Pick a card
+    /**
+     * Creates a list of cards from hand that can be played on top of the discard deck
+     * 
+     * @param discardDeck The deck of cards being used to store the cards
+     *      being discarded during the game
+     * @return The list of cards in queue to be used for current turn
+     */
+    private ArrayList<Card> findPlayableCards(DiscardDeck discardDeck) {
+        Card lastPlayedCard = discardDeck.getTopOfDeck();
+
         ArrayList<Card> playable = new ArrayList<Card>();
-        // create a list of playable cards
         think();
-        for (Card card : handCards) {
-            if (card.equals(lastPlayed)) {
+        for (Card card : cardsInHand) {
+            if (card.equals(lastPlayedCard)) {
                 playable.add(card);
             }
         }
-        String action = "";
-        if (playable.size() == 0) {
-            think();
+
+        return playable;
+    }
+
+    /**
+     * Draws from the playing deck and immediately puts the drawn card into the list
+     * of cards to use for current move
+     * 
+     * @param playingDeck The deck of cards being drawn from
+     * @param playableCards The list of cards in queue to be used for current turn
+     */
+    private void drawFromDeck(PlayingDeck playingDeck, ArrayList<Card> playableCards) {
+        think();
             System.out.println("Had no playable card in hand, drawing from deck...");
             Card drawn = playingDeck.drawCard();
             drawn.flipCard();
-            playable.add(drawn);
+            playableCards.add(drawn);
+    }
+
+    /**
+     * Picks the first card in the list of playable cards
+     * 
+     * @return The card chosen from the list of playable cards
+     */
+    private Card pickCard(String action, ArrayList<Card> playableCards, PlayingDeck playingDeck) {
+        if (playableCards.size() == 0) {
+            drawFromDeck(playingDeck, playableCards);
             action += "Drew a ";
-        } else {
-            // action += "Picked";
-        }
-        // choose the best card to play
-        Card chosenCard = playable.get(0);
+        } 
+        Card chosenCard = playableCards.get(0);
         if (action.equals("Drew a ")) {
             System.out.print(action + chosenCard + " card, ");
         }
+        return chosenCard;
+    }
 
-        for (Card card : handCards) {
+    /**
+     * Removes the card chosen from the playable cards list from the Computer Player's 
+     * hand
+     * 
+     * @param chosenCard The card chosen from the list of playable cards 
+     */
+    private void discardFromHand(Card chosenCard) {
+        for (Card card : cardsInHand) {
             if (chosenCard.toString().equals(card.toString())) {
                 //System.out.println("Playing " + card);
-                handCards.remove(card);
+                cardsInHand.remove(card);
                 break;
             }
         }
+    }
+
+    /**
+     * Changes the color of the card last played. Used in cases where a Computer
+     * Player just played a Wild Card or a Plus Four Card.
+     * The Computer changes the color to the first color in their hand
+     * 
+     * @param discardDeck The deck of cards being used to store the cards
+     *      being discarded during the game
+     */
+    private void changeSpecialCardColor(DiscardDeck discardDeck){
+        char color = 's';
+            for (Card card : cardsInHand) {
+                if (card.getColor() != 's')
+                    color = cardsInHand.get(0).getColor();
+            }
+            Card lastPlayedCard = discardDeck.getTopOfDeck();
+
+            think();
+            System.out.print("Changing the color to ");
+            switch (color) {
+                case 'r':
+                    System.out.println("\033[31;1mred\033[0m");
+                    break;
+                case 'b':
+                    System.out.println("\033[34;1mblue\033[0m");
+                    break;
+                case 'g':
+                    System.out.println("\033[32;1mgreen\033[0m");
+                    break;
+                case 'y':
+                    System.out.println("\033[33;1myellow\033[0m");
+                    break;
+            }
+            lastPlayedCard.changeColor(color);
+    }
+
+    /**
+     * {@inheritDoc}}.
+     * First the computer creates a list of cards they can play for their turn.
+     * If they have no playable cards they draw from the playing deck and attempts
+     * to play the drawn card.
+     */
+    @Override
+    public Card makeMove(PlayingDeck playingDeck, DiscardDeck discardDeck) {
+        ArrayList<Card> playableCards = findPlayableCards(discardDeck);
+        String action = "";
+
+        Card chosenCard = pickCard(action, playableCards, playingDeck);
+        discardFromHand(chosenCard);
+
         think();
         if (chosenCard.canPlayCard(discardDeck, this)) {
             // cases where a color changing card is played
             if (discardDeck.getTopOfDeck().getValue() > 12) {
-                char color = 's';
-                for (Card card : handCards) {
-                    if (card.getColor() != 's')
-                        color = handCards.get(0).getColor();
-                }
-                Card discardTop = discardDeck.getTopOfDeck();
-                think();
-                System.out.print("Changing the color to ");
-                switch (color) {
-                    case 'r':
-                        System.out.println("\033[31;1mred\033[0m");
-                        break;
-                    case 'b':
-                        System.out.println("\033[34;1mblue\033[0m");
-                        break;
-                    case 'g':
-                        System.out.println("\033[32;1mgreen\033[0m");
-                        break;
-                    case 'y':
-                        System.out.println("\033[33;1myellow\033[0m");
-                        break;
-                }
-                discardTop.changeColor(color);
+                changeSpecialCardColor(discardDeck);
             }
-        } else {
-            System.out.println("But couldn't play the " +chosenCard + " card");
+        } 
+        else {
+            System.out.println("But couldn't play the " + chosenCard + " card");
             chosenCard = null;
         }
         return chosenCard;
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
-    boolean isHuman() {
+    public boolean isHuman() {
         return false;
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
-    String displayHand() {
+    public String displayHand() {
         String output = "";
-        for (Card card : handCards) {
+        for (Card card : cardsInHand) {
             card.flipCard();
             output += card;
             card.flipCard();
@@ -109,7 +182,11 @@ public class Computer extends Player {
         return output;
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
-    void gameOverAction() {
+    public void gameOverAction() {
+        System.out.println("Good Game");
     }
 }
